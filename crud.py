846935e48419611150c1e9b1133ebe9941fdb649
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-07-14 21:00:31 -0700

    Created functions: create_user, create_movie, create_rating

---
+++ crud.py
@@ -0,0 +1,42 @@
+"""CRUD operations"""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(
+        title = title,
+        overview = overview,
+        release_date = release_date,
+        poster_path = poster_path,
+    )
+
+    return movie
+
+
+def create_rating(score, movie_id, user_id):
+    """Create and return a new rating."""
+    
+    rating = Rating(
+        score = score,
+        movie_id = movie_id,
+        user_id = user_id,
+    )
+
+    return rating
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+
