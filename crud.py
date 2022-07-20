cb3b06bec164c57ea05688461035591236236fb3
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-07-19 19:54:42 -0700

    Completed Part 3: Set up server.py, homepage, can display all users and movies

--- crud.py
+++ crud.py
@@ -36,6 +36,31 @@ def create_rating(score, movie_id, user_id):
     return rating
 
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+    
+
+def get_movie_by_id(movie_id):
+    """Return a movie's ID."""
+
+    return Movie.query.get(movie_id)
+
+
+def get_users():
+    """ Return all users """
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return a user's ID."""
+
+    return User.query.get(user_id)
+
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
