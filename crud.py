1a453f6db08cc9d08e400e498c841c4ee2cae790
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-07-19 21:40:06 -0700

    Part 4: Implemented User Registration and Login

--- crud.py
+++ crud.py
@@ -59,6 +59,11 @@ def get_user_by_id(user_id):
 
     return User.query.get(user_id)
 
+def get_user_by_email(email):
+    """Return a user by email"""
+
+    return User.query.filter(User.email == email).first()
+
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -50,7 +50,41 @@ def show_user(user_id):
     user = crud.get_user_by_id(user_id)
     return render_template('user_details.html', user = user)
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    """ Create a new user """
 
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash("Cannot create an account with that email. Try again.")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+
+    return redirect('/')
+
+@app.route("/login", methods=["POST"])
+def user_login():
+    """Allows user to log in"""
+
+  
+    email = request.form.get("email")   
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect.")
+    else:
+        session["user_email"] = user.email
+        flash(f"Welcome back, {user.email}!")
+
+    return redirect("/")
+    
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
